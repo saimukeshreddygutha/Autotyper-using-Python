f01c31087b61354ebc431eeea5d15cbc1826ce90
saimukeshreddygutha <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-02-01 23:47:22 +0530

    Create Autotyper.py

---
+++ Autotyper.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+time.sleep(10)
+pyautogui.typewrite("""PyAutoGUI lets your Python scripts control the mouse and keyboard to automate interactions with other applications.
+The API is designed to be simple. PyAutoGUI works on Windows, macOS, and Linux, and runs on Python 2 and 3.""",interval=0.3)
